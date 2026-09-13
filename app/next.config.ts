09b6a9634a3ea6c4a45463aafff98232817d8aff
Fred <fred@example.com>
2026-09-13 08:03:03 +0200

    Remove distDir

--- app/next.config.ts
+++ app/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  distDir: "../.next",
   images: {
     formats: ["image/avif", "image/webp"],
   },
